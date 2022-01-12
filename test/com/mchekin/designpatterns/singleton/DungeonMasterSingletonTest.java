5a7b79ddbd28c94fd3dd77f05609045571e475f5
Michael Chekin <mchekin@gmail.com>
2022-01-12 21:53:21 +0100

    Renaming sut to dungeonMaster in DungeonMasterSingletonTest

--- test/com/mchekin/designpatterns/singleton/DungeonMasterSingletonTest.java
+++ test/com/mchekin/designpatterns/singleton/DungeonMasterSingletonTest.java
@@ -8,11 +8,11 @@ class DungeonMasterSingletonTest {
 
     @Test
     void sameInstanceForConsequentGetInstanceCalls() {
-        DungeonMasterSingleton sut1 = DungeonMasterSingleton.getInstance("John");
-        DungeonMasterSingleton sut2 = DungeonMasterSingleton.getInstance("Jim");
+        DungeonMasterSingleton dungeonMaster1 = DungeonMasterSingleton.getInstance("John");
+        DungeonMasterSingleton dungeonMaster2 = DungeonMasterSingleton.getInstance("Jim");
 
-        assertEquals("John", sut1.getName());
-        assertEquals("John", sut2.getName());
-        assertSame(sut1, sut2);
+        assertEquals("John", dungeonMaster1.getName());
+        assertEquals("John", dungeonMaster2.getName());
+        assertSame(dungeonMaster1, dungeonMaster2);
     }
 }
